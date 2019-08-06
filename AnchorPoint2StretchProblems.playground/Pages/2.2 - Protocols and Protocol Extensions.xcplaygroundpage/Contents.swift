17d9c29f77a929f5737e7c927915ba350ea9fab4
Sambo <flatheadcatfish9@yahoo.com>
2019-08-06 09:46:36 -0600

    Tuesday Stretch

--- AnchorPoint2StretchProblems.playground/Pages/2.2 - Protocols and Protocol Extensions.xcplaygroundpage/Contents.swift
+++ AnchorPoint2StretchProblems.playground/Pages/2.2 - Protocols and Protocol Extensions.xcplaygroundpage/Contents.swift
@@ -14,10 +14,99 @@
  - Create two more classes that conform to `Racer` or make your existing vehicles conform to `Racer`. Create another function outside of your classes that will take in two `Racer` objects and return the winner.
  */
 import Foundation
+protocol Vehicle: class {
+    var speed: Double { get }
+    var isMoving: Bool { get set }
+    func startMoving()
+    func stopMoving()
+}
+extension Vehicle {
+    func startMoving() {
+        if isMoving == true {
+//            isMoving = false
+            isMoving.toggle()
+            print("Stopping...")
+        } else {
+//            isMoving = true
+            isMoving.toggle()
+            print("We're moving now")
+        }
+    }
+    func stopMoving() {
+        if isMoving == false {
+//            isMoving = true
+            isMoving.toggle()
+            print("We're moving again")
+        } else {
+//            isMoving = false
+            isMoving.toggle()
+            print("Stopping...")
+        }
+    }
+}
+class LawnMower: Vehicle {
+    var speed: Double
+    var isMoving: Bool
+    
+    init(speed: Double, isMoving: Bool) {
+        self.speed = speed
+        self.isMoving = isMoving
+    }
+    
+    
+}
+class Truck: Vehicle {
+    var speed: Double
+    var isMoving: Bool
+    
+    init(speed: Double, isMoving: Bool) {
+        self.speed = speed
+        self.isMoving = isMoving
+    }
+    
+    
+}
+let johnDeer = LawnMower(speed: 50, isMoving: true)
+let tundra = Truck(speed: 15, isMoving: false)
+johnDeer.isMoving
+johnDeer.stopMoving()
+johnDeer.startMoving()
+tundra.isMoving
+tundra.startMoving()
+tundra.stopMoving()
 
-
-
-
-
-
+protocol Racer: Vehicle {
+    func race() -> Double
+}
+//
+//extension Racer {
+//    func race() {
+//        print("It'll take \(1000 / speed) moments to travel 1000 meters")
+//    }
+//}
+class Porsche: Racer {
+    func race() -> Double {
+        print("It'll take \(1000 / speed) moments to travel 1000 meters")
+        return 1000 / speed
+    }
+    
+    var speed: Double
+    var isMoving: Bool
+    
+    init(speed: Double, isMoving: Bool) {
+        self.speed = speed
+        self.isMoving = isMoving
+    }
+}
+let nineFourtyFour = Porsche(speed: 500, isMoving: true)
+nineFourtyFour.race()
+let nineEleven = Porsche(speed: 1000, isMoving: true)
+func winner(racerOne: Double, racerTwo: Double) -> String {
+    if racerOne > racerTwo {
+        return "racer 2 wins"
+    } else {
+        return "racer 1 wins"
+    }
+}
+winner(racerOne: nineFourtyFour.race(), racerTwo: nineEleven.race())
 //: [Number Printer](@next)
